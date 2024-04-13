ebdbb1064412b781a427681e2396b3552599a60b
SAJacob <113067165+SAJacob@users.noreply.github.com>
2024-04-12 23:34:31 -0500

    Delete wishmate_login2.py

--- wishmate_login2.py
+++
@@ -1,9 +0,0 @@
-from kivy.lang import Builder
-from kivymd.app import MDApp
-class MainApp(MDApp):
-    def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Red"
-        self.theme_cls.accent_palette = "Blue"
-        return Builder.load_file('login2.kv')
-    
