d0ca1a49c010f52025890f476c7932a8716094e5
KusumaMurthy109 <116034874+KusumaMurthy109@users.noreply.github.com>
2024-04-14 02:10:06 -0500

    Delete readfile.py

--- readfile.py
+++
@@ -1,9 +0,0 @@
-class ReadFile:
-    def read(self, file):
-        file = open(file, 'r')
-        list1 = []
-        for line in file:
-            list1 = line.strip().split(',')
-        
-        return list1
-
