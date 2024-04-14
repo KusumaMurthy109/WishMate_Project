cd29a1ba5b797197337eb1d53c1763a2e0058038
KusumaMurthy109 <116034874+KusumaMurthy109@users.noreply.github.com>
2024-04-14 03:38:16 -0500

    Update linksrecommender.py

--- linksrecommender.py
+++ linksrecommender.py
@@ -1,12 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 
-class LinksRecommender:
+class LinksRecommender: #scrapes the internet to search for recommended links
     def links(self, word):
-        link = f"https://www.google.com/search?q={word}"
+        link = f"https://www.google.com/search?q={word}" #google links
         dictionary = {"key": "value"}
         answer = requests.get(link, dictionary) 
-        bs4 = BeautifulSoup(answer.text, 'html.parser')
+        bs4 = BeautifulSoup(answer.text, 'html.parser') #use the beautiful soup software
         link_list = []
         for link in bs4.find_all('a'):
             href = link.get('href')
@@ -19,7 +19,7 @@ class LinksRecommender:
             if word in link:
                 sort.append(link)
         
-        return sort
+        return sort #return the list of links
 
 
                 
