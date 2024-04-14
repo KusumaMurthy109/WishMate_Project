5f0b9b88c46e054a2b0fc2ed5e8fb11214ca94f6
linann260 <106176130+linann260@users.noreply.github.com>
2024-04-14 03:41:17 -0500

    commented profiledb.py

--- profiledb.py
+++ profiledb.py
@@ -1,15 +1,24 @@
 import pymongo
 
 class ProfileDB:
+    '''
+    Creates ProfileDB class that connects to WishMate_Profiles Database.
+    '''
 
     def __init__(self):
+        '''
+        Define Member Variables.
+        '''
         self._client = pymongo.MongoClient("mongodb+srv://annalin260:12345@cluster0.rtdi4fq.mongodb.net/")
         self._profile_db = self._client["WishMate_Profiles"]
         self._wishlist_db = self._client["WishMate_Wishlists"]
         self.userCol = self._profile_db["Users"]
 
-    #WishMate_Profiles Database
     def createAccount(self, username, password, firstname, lastname, zipcode):
+        '''
+        Creates an account for new users.
+        '''
+        #Validates that user is a new user to the database. Returns True if accounts is successfully created.
         try:
             self.userCol.insert_one({"_id": username,
                                      "First Name": firstname,
@@ -21,8 +30,11 @@ class ProfileDB:
         except pymongo.errors.DuplicateKeyError:
             return False
 
-    #WishMate_Profiles Database
     def userLogin(self, username, password):
+        '''
+        Validates user login.
+        '''
+        #Validates user's username and password upon login. Returns True if login successful.
         cursor = self.userCol.find({"_id": username, "Password": password})
         documents = list(cursor)
         if len(documents) == 0:
